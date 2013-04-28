3a7a7e649eabe0ba62156dc4600d209d3d487098
deksan <deksan@gmail.com>
2013-04-28 10:57:02 +0200

    Added configuration with website and apikey support. Started TV

--- flexget/plugins/urlrewrite_newznab.py
+++ flexget/plugins/urlrewrite_newznab.py
@@ -2,6 +2,7 @@ __author__ = 'deksan'
 
 import logging
 import feedparser
+import urllib, urllib2, urlparse, httplib
 
 from flexget import validator
 from flexget.entry import Entry
@@ -17,23 +18,60 @@ log = logging.getLogger('newznab')
 
 
 class Newznab(object):
+    """
+        Newznab urlrewriter
+        Provide a url or your webiste + apikey
+        
 
+        movies:
+          movie_queue: yes
+          newznab:
+            url: "http://website/api?apikey=xxxxxxxxxxxxxxxxxxxxxxxxxx&t=movie&extended=1"
+            website: https://website
+            apikey: xxxxxxxxxxxxxxxxxxxxxxxxxx
+            category: movie
+          
+        Category is any of: movie, tvsearch, music, book
+    """
     def validator(self):
         """Return config validator."""
         root = validator.factory('dict')
-        root.accept('url', key='url', allow_replacement=True)
-        root.accept('choice', key='category').accept_choices(['all', 'movies', 'tv', 'music', 'books', 'xxx', 'other'])
+        root.accept('url', key='url', required=False)
+        root.accept('url', key='website', required=False)
+        root.accept('text', key='apikey', required=False)
+        root.accept('choice', key='category', required=True).accept_choices(['movie', 'tvsearch', 'tv', 'music', 'book'])
         return root
 
+    def build_config(self,config):
+        if ( config['category'] == 'tv'):
+            config['category'] = 'tvsearch'
+        if ( 'url' not in config ):
+            if ( 'apikey' in config and 'website' in config ):
+                params = {
+                    "t":config['category'],
+                    "apikey": config['apikey'],
+                    "extended": 1
+                }
+                config['url']=config['website']+'/api?'+urllib.urlencode(params)
+        return config
+
     def on_task_input(self, task, config):
-        if config["category"] == "movies":
-            return self.on_task_input_movies(task, config)
+        config = self.build_config(config)
+        if config["category"] == "movie":
+            return self.on_task_input_movie(task, config)
+        elif config["category"] == "tvsearch":
+            return self.on_task_input_tvsearch(task, config)
         else:
             entries = []
             log.warning("Not done yet...")
             return entries
 
-    def on_task_input_movies(self, task, config):
+    def on_task_input_tvsearch(self, task, config):
+        entries = []
+        
+        return entries
+
+    def on_task_input_movie(self, task, config):
         entries = []
         quality_plugin = get_plugin_by_name('metainfo_quality')
         for queue_item in queue_get():
@@ -41,32 +79,36 @@ class Newznab(object):
                 imdb_id = queue_item.imdb_id.replace('tt', '')
                 log.info('Searching for %s ( %s )' % (imdb_id, queue_item.quality ))
                 url = config['url'] + "&imdbid=" + imdb_id
-                log.info(url)
-                rss = feedparser.parse(url)
-                status = rss.get('status', False)
-                if status != 200:
-                    raise PluginWarning('Search result not 200 (OK), received %s' % status)
+                log.debug(url)
 
-                req = quals.Requirements(queue_item.quality)
-                for rss_entry in rss.entries:
-                    entry = Entry()
-                    for key in rss_entry.keys():
-                        entry[key] = rss_entry[key]
-                    entry["url"] = entry["link"]
-                    entry.register_lazy_fields(['quality'], quality_plugin.instance.lazy_loader)
-                    if req.allows(entry['quality']):
-                        log.info('{%s} {%s}' % (entry['quality'], entry['title']))
-                        entries.append(entry)
-                    # todo : add newznab attributes such as size
+                try:
+                    data = urllib2.urlopen(url, timeout=20).read()
+                except urllib2.URLError, e:
+                    logger.warn('Error fetching data from newznab provider: %s' % e)
+                    data = False
+
+                if data:
+                    rss = feedparser.parse(data)
+
+                    if not len(rss.entries):
+                        log.info("No results returned")
+                    req = quals.Requirements(queue_item.quality)
+                    for rss_entry in rss.entries:
+                        entry = Entry()
+                        for key in rss_entry.keys():
+                            entry[key] = rss_entry[key]
+                        entry["url"] = entry["link"]
+                        entry.register_lazy_fields(['quality'], quality_plugin.instance.lazy_loader)
+                        if req.allows(entry['quality']):
+                            log.debug('{%s} {%s}' % (entry['quality'], entry['title']))
+                            entries.append(entry)
+                        else:
+                            log.debug('refused quality {%s} {%s}' % (entry['quality'], queue_item.quality))
+                        
+                        # todo : add newznab attributes such as size
 
         return entries
 
-    def search(self, query, comparator, config=None):
-        comparator.set_seq1(query)
-        log.info("Helloooooo")
-        log.info(config['url'])
-        log.info(config['category'])
-        log.info("Looping")
-        log.info(query)
+   
 
 register_plugin(Newznab, 'newznab', api_ver=2)
